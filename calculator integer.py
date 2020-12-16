69f05efbd6922f8228850adefb27cc8dd8aaa801
keyj <74073784+programkeyj@users.noreply.github.com>
2020-12-16 18:43:28 +0200

    int integer

---
+++ calculator integer.py
@@ -0,0 +1,22 @@
+for h in range(99999999999):
+     print("вводите первое число")
+     a = int(input())
+     print("вводите второе число")
+     b = int(input())
+     print("+ or -")
+     c = input('')
+     if c != '+':
+        print(a - b)
+     if c == '+':
+        print(a + b)
+
+
+
+
+
+
+
+
+
+
+
